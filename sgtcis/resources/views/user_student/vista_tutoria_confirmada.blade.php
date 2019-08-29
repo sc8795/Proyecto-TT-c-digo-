47993d7ed6bb776121fe9aed436a75f80dc26706
sc8795 <sdcartuchem@unl.edu.ec>
2019-08-28 22:54:57 -0500

    Diseño en la vista de tutoría confirmada - estudiante
    Se realizaron unos controles para la interfaz del estudiante cuando el docente le ha confirmado la tutoría solicitada, además del diseño al presentar el aviso de evaluación al docente.

--- sgtcis/resources/views/user_student/vista_tutoria_confirmada.blade.php
+++ sgtcis/resources/views/user_student/vista_tutoria_confirmada.blade.php
@@ -18,7 +18,15 @@
                     @php
                         $fecha_tutoria=$datos_tut->fecha_tutoria;
                         $date = date_create($fecha_tutoria);
-                        $fecha=date_format($date, 'd-m-Y');
+                        $fecha_tutoria=date_format($date, 'd-m-Y');
+                        
+                        $fecha_actual=now();
+                        $date = date_create($fecha_actual);
+                        $fecha_actual=date_format($date, 'd-m-Y');
+
+                        $hora_actual=date("G");
+                        $minuto_actual=date("i");
+                                    
                     @endphp
                     <h6 class="negrita">La tutoría será realizada el día: 
                         <span class="quita_negrita">{{$datos_tut->dia}} de 
@@ -35,20 +43,63 @@
                             @else
                                 {{$datos_tut->minutos_fin}}
                             @endif
-                            , el <span class="negrita">{{$fecha}}</span>.
+                            , el <span class="negrita">{{$fecha_tutoria}}</span>.
                         </span>
                     </h6>
                     <h6 class="negrita">Lugar de tutoría: <span class="quita_negrita">Salón de clases</span></h6>
                     <h6 class="negrita">Motivo a tratar: <span class="quita_negrita">{{$datos_tut->motivo}}</span></h6>
                     <hr>
-                    <div class="alert alert-primary" role="alert">
-                        <h6>
-                            <span class="negrita">¡Aviso!</span>
-                            Una vez recibida la tutoría en el día acordado, el proceso finalizará cuando evalúe al docente sobre la tutoría impartida.
-                            <br><br>
-                            <a href="{{url("evaluar_docente/".auth()->user()->id."/{$notification}/{$datos_tut->id}/{$materia->id}/{$docente->id}")}}" class="btn btn-sm btn-outline-success">Evaluar docente</a>
-                        </h6>
-                    </div>
+                    @if ($fecha_tutoria==$fecha_actual)
+                        @if ($datos_tut->hora_fin == $hora_actual)
+                            @if ($datos_tut->minutos_fin > $minuto_actual)
+                                <div class="alert alert-danger">
+                                    <p>¡Aviso! El proceso finalizará cuando evalúe al docente sobre la tutoría recibida. El botón para evaluar al docente se activará una vez haya recibido 
+                                        la tutoría en el día acordado. Por favor asegúrese de volver entrar a este enlace y evaluar al docente. 
+                                    </p>
+                                    <button type="button" class="hint--top btn-default" id="borde_radio" data-hint="Evaluar docente - desactivado" disabled="disabled">Evaluar docente</button>
+                                </div>
+                            @else
+                                <div class="alert alert-success">
+                                    <p>¡Aviso! El proceso finalizará cuando evalúe al docente sobre la tutoría recibida. El botón para evaluar al docente se encuentra activado.
+                                        Por favor asegúrese de evaluar al docente haciendo clic en el botón "Evaluar docente".
+                                    </p>
+                                    <a href="{{url("evaluar_docente/".auth()->user()->id."/{$notification}/{$datos_tut->id}/{$materia->id}/{$docente->id}")}}" class="btn btn-outline-dark">Evaluar docente <span class="fas fa-check-circle"></span></a>
+                                </div>
+                            @endif
+                        @else
+                            @if ($datos_tut->hora_fin > $hora_actual)
+                                <div class="alert alert-danger">
+                                    <p>¡Aviso! El proceso finalizará cuando evalúe al docente sobre la tutoría recibida. El botón para evaluar al docente se activará una vez haya recibido 
+                                        la tutoría en el día acordado. Por favor asegúrese de volver entrar a este enlace y evaluar al docente. 
+                                    </p>
+                                    <button type="button" class="hint--top btn-default" id="borde_radio" data-hint="Evaluar docente - desactivado" disabled="disabled">Evaluar docente</button>
+                                </div>
+                            @else
+                                <div class="alert alert-success">
+                                    <p>¡Aviso! El proceso finalizará cuando evalúe al docente sobre la tutoría recibida. El botón para evaluar al docente se encuentra activado.
+                                        Por favor asegúrese de evaluar al docente haciendo clic en el botón "Evaluar docente".
+                                    </p>
+                                    <a href="{{url("evaluar_docente/".auth()->user()->id."/{$notification}/{$datos_tut->id}/{$materia->id}/{$docente->id}")}}" class="btn btn-outline-dark">Evaluar docente <span class="fas fa-check-circle"></span></a>
+                                </div>
+                            @endif
+                        @endif
+                    @else
+                        @if ($fecha_tutoria > $fecha_actual)
+                            <div class="alert alert-danger">
+                                <p>¡Aviso! El proceso finalizará cuando evalúe al docente sobre la tutoría recibida. El botón para evaluar al docente se activará una vez haya recibido 
+                                    la tutoría en el día acordado. Por favor asegúrese de volver entrar a este enlace y evaluar al docente. 
+                                </p>
+                                <button type="button" class="hint--top btn-default" id="borde_radio" data-hint="Evaluar docente - desactivado" disabled="disabled">Evaluar docente</button>
+                            </div>
+                        @else
+                            <div class="alert alert-success">
+                                <p>¡Aviso! El proceso finalizará cuando evalúe al docente sobre la tutoría recibida. El botón para evaluar al docente se encuentra activado.
+                                    Por favor asegúrese de evaluar al docente haciendo clic en el botón "Evaluar docente".
+                                </p>
+                                <a href="{{url("evaluar_docente/".auth()->user()->id."/{$notification}/{$datos_tut->id}/{$materia->id}/{$docente->id}")}}" class="btn btn-outline-dark">Evaluar docente <span class="fas fa-check-circle"></span></a>
+                            </div>
+                        @endif
+                    @endif
                     <br>
                 </div> 
             </div>
